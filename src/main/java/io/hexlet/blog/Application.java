7405108082c9a877782fbd8f39db76346533f217
artmper <twistertwist1@gmail.com>
2026-09-24 02:23:44 +0300

    add: set http status codes via ResponseEntity

--- src/main/java/io/hexlet/blog/Application.java
+++ src/main/java/io/hexlet/blog/Application.java
@@ -4,13 +4,24 @@ import io.hexlet.blog.model.Post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -32,24 +43,33 @@ public class Application {
     }
 
     @GetMapping("/posts")
-    public List<Post> index(@RequestParam(defaultValue = "5") Integer limit) {
-        return posts.stream().limit(limit).toList();
+    public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "5") Integer limit) {
+        var limitedPosts = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok(limitedPosts);
     }
 
     @GetMapping("/posts/{id}")
-    public Optional<Post> show(@PathVariable String id) {
+    public ResponseEntity<Post> show(@PathVariable String id) {
         var post = posts.stream().filter(p -> p.getSlug().equals(id)).findFirst();
-        return post;
+
+        return ResponseEntity.of(post);
     }
 
-    @PostMapping("/posts") // Создание страницы
-    public Post create(@RequestBody Post post) {
+    @PostMapping("/posts")
+    public ResponseEntity<Post> create(@RequestBody Post post) {
         posts.add(post);
-        return post;
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(post.getSlug())
+                .toUri();
+
+        return ResponseEntity.created(location).body(post);
     }
 
     @PutMapping("/posts/{id}")
-    public Post update(@RequestBody Post data, @PathVariable String id) {
+    public ResponseEntity<Post> update(@RequestBody Post data, @PathVariable String id) {
         var post = posts.stream()
                 .filter(p -> p.getSlug().equals(id))
                 .findFirst()
@@ -59,13 +79,13 @@ public class Application {
         post.setAuthor(data.getAuthor());
         post.setContent(data.getContent());
         post.setTitle(data.getTitle());
-        post.setCreatedAt(data.getCreatedAt());
 
-        return post;
+        return ResponseEntity.ok(post);
     }
 
     @DeleteMapping("/posts/{id}")
-    public void destroy(@PathVariable String id) {
+    public ResponseEntity<Void> destroy(@PathVariable String id) {
         posts.removeIf(p -> p.getSlug().equals(id));
+        return ResponseEntity.noContent().build();
     }
 }
